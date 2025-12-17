f9173e43fe2c6aafcf90a9e774f43b404687b636
ArtefactumSK <artefactum@artefactum.sk>
2025-12-17 11:42:32 +0100

    Zapojenie kontroleru

--- spa-core.php
+++ spa-core.php
@@ -17,5 +17,7 @@ require_once __DIR__ . '/includes/services/RegistrationService.php';
 if (!class_exists('SPA_Registration_Service')) {
     error_log('[SPA CORE] RegistrationService class NOT loaded');
 }
+require_once __DIR__ . '/includes/controllers/GravityRegistrationController.php';
+
 
 register_activation_hook(__FILE__, 'spa_core_install_db');
